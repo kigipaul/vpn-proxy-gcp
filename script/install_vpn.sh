cc5ced7b9d0815039253e9ce00ba95918d5e618f
kigipaul <kigipaul@kigi-VPS.(none)>
2018-05-29 18:51:36 +0800

    Change startup script method

--- script/install_vpn.sh
+++ script/install_vpn.sh
@@ -3,7 +3,6 @@ if [ ! -e "!CLIENT_CONFIG!" ]; then
     echo "No such file: !CLIENT_CONFIG!"
     exit 1
 fi
-sudo su -
 cd /root
 git clone https://github.com/kigipaul/vpn-proxy-ovpn.git vpn-proxy
 cp -r !CLIENT_CONFIG! vpn-proxy/config/remote
